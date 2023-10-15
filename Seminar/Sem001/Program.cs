000e18781d9a56d86417aa50aa00024594b20422
Giovanni2810 <V_crazy28@mail.ru>
2023-10-15 16:15:03 +0300

    add Sem 001

--- Seminar/Sem001/Program.cs
+++ Seminar/Sem001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Csharp!");
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.Write("Введите значение: ");                  // Приглашаю пользователя ко вводу
+int number = Convert.ToInt32(Console.ReadLine()); // Принимаем число от пользователя и помещаем в переменную number
+  // int number1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine(number);                // Выводим переменную number
+int sqr = number * number;                      // Вычислили квадрат числа и поместили значение в переменную в sqr
+Console.WriteLine(sqr);                        // Выводим ответ
